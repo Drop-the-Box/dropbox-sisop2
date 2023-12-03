0cb1f0c80add1e295e22db156ad51902f98901d1
Vagner Coimbra <vagnercoimbra@outlook.com>
2023-12-03 19:56:08 -0300

    Ajustes de codigos

--- src/inotify/test.cpp
+++ src/inotify/test.cpp
@@ -5,9 +5,9 @@ int main() {
     Inotify inotify("/home/teste");
     
     int file_descriptor = inotify.get_file_descriptor();
-    std::cout << "file_descriptor: " << file_descriptor << std::endl;
+    cout << "file_descriptor: " << file_descriptor << endl;
     int watch_descriptor = inotify.get_watch_descriptor();
-    std::cout << "watch_descriptor: " << watch_descriptor << std::endl;
+    cout << "watch_descriptor: " << watch_descriptor << endl;
 
     while (true) {
         inotify.read_event();
