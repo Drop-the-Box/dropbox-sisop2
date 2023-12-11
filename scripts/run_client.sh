5fbed51b37db00055dcb32c41a99ea5ed50ac691
Alisson <itsalissom@gmail.com>
2023-12-10 22:44:54 -0300

    fix: scripts and map errors

--- scripts/run_client.sh
+++ scripts/run_client.sh
@@ -9,5 +9,8 @@ elif [ "$DEBUG_MEM" == "true" ]; then
     valgrind --leak-check=yes ./bin/client $1 $2 $3;
 else
     g++ -I src/include src/client/**/*.cpp src/common/**/*.cpp src/client/main.cpp -o ./bin/client;
+    echo $1;
+    echo $2;
+    echo $3;
     ./bin/client $1 $2 $3;
 fi
